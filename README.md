84f1935aefdd2cfdc4c51d3e4f7b9458cef8ed0b
semreh17 <sputo99@gmail.com>
2025-10-15 14:30:33 +0200

    selfie

---
+++ README.md
@@ -0,0 +1,10 @@
+## selfie
+
+```
+sudo apt update
+sudo apt install fswebcam
+```
+```
+gcc selfie.c -o selfie
+sudo mv selfie /usr/local/bin/
+```
---
+++ selfie.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void cameraDraw() {
+    printf("            ___ \n");
+    printf("           / _ \\ \n");
+    printf("          | / \\ | \n");
+    printf("          | \\_/ | \n");
+    printf("           \\___/ ___ \n");
+    printf("           _|_|_/[_]\\__==_ \n");
+    printf("          [---------------] \n");
+    printf("          | O   /---\\     | \n");
+    printf("          |    |     |    | \n");
+    printf("          |     \\___/     | \n");
+    printf("          [---------------] \n");
+    printf("                [___] \n");
+    printf("                 | |\\\\ \n");
+    printf("                 | | \\\\ \n");
+    printf("                 [ ]  \\\\_ \n");
+    printf("                /|_|\\  ( \\ \n");
+    printf("               //| |\\\\  \\ \\ \n");
+    printf("              // | | \\\\  \\ \\ \n");
+    printf("             //  |_|  \\\\  \\_\\ \n");
+    printf("            //   | |   \\\\ \n");
+    printf("           //\\   | |   /\\ \\ \n");
+    printf("          //  \\  | |  /  \\ \\ \n");
+    printf("         //    \\ | | /    \\ \\ \n");
+    printf("        //      \\|_|/      \\ \\ \n");
+    printf("       //        [_]        \\ \\ \n");
+    printf("      //          H          \\ \\ \n");
+    printf("     //           H           \\ \\ \n");
+    printf("    //            H            \\ \\ \n");
+    printf("   //             H             \\ \\ \n");
+    printf("  //              H              \\ \\ \n");
+    printf(" //                               \\ \\ \n");
+    printf("//                                 \\ \\ \n");
+    return;
+}
+
+
+int main() {
+    // Comando per fswebcam: scatta 1 frame, salvalo come selfie.jpg
+    const char *command_format = "fswebcam -r 1280x720 --frames 1 %s";
+    char command[256];
+    const char *filename = "selfie.jpg";
+    snprintf(command, sizeof(command), command_format, filename);
+    
+    cameraDraw();
+
+    system(command);
+    system("open selfie.jpg");
+    return EXIT_SUCCESS;
+}
